96ae8448252d1b51f4785cd2a9bb5e40b34c2054
Arthur <pennywennyweed@gmail.com>
2025-08-09 23:33:53 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script>
+import { BButton } from 'bootstrap-vue-next'
+export default {
+  components: { BButton },
+}
+</script>
+<template>
+  <BButton variant="outline-primary">Button</BButton>
+</template>
